d94eb79265e540b373c688aa790fd2b12179a25e
Aldo Okware <aldo.okware@moap.co.ug>
2019-11-12 01:04:22 +0000

    implemented ci/cd with circle ci / refactor

---
+++ .circleci/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+sudo apt-get update -y
+sudoe apt-get upgrade 
+
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+export PATH=~/.npm-global/bin:$PATH
+source ~/.profile
+npm install -g serverless
+
+# set credentials
+sls config credentials --provider aws --key $AWS_ACCESS_KEY_ID  --secret $AWS_SECRET_ACCESS_KEY
