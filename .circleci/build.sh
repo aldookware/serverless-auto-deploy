d2f623ed25fad980ce7eeee90eec7aa352dc2011
Aldo Okware <aldo.okware@moap.co.ug>
2019-11-12 02:57:31 +0000

    cleanup

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 sudo apt-get update -y
 sudo apt-get upgrade 
-sudo apt-get install make
 
+# setup node and install serverless framework.
+# TODO: This way of install node is not the best we need a better way.
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 export PATH=~/.npm-global/bin:$PATH
