4f1ed74f840f4a1225b4655e112d8c0fab869157
aldo okware <aldo.okware@moap.co.ug>
2020-02-08 04:08:19 +0300

    remove sudo from .sh file

--- layer/build_layer.sh
+++ layer/build_layer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export PKG_DIR="python"
 rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR}
-sudo docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.7 \
+docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.7 \
     pip install -r ./requirements.txt --no-deps -t ${PKG_DIR}
 cd .. 
 pwd
