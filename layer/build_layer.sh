29e0957e1f434d76ebdbba8e6ee7a957c603ac33
aldo okware <aldo.okware@moap.co.ug>
2020-02-08 03:42:45 +0300

    add sudo to docker command

--- layer/build_layer.sh
+++ layer/build_layer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export PKG_DIR="python"
 rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR}
-docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.7 \
+sudo docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.7 \
     pip install -r ./requirements.txt --no-deps -t ${PKG_DIR}
 cd .. 
 pwd
